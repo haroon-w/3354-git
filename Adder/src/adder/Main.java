f77b63591f79695de2d4d4ffd5fdb45ffa4aad34
Haroon Wahab <haroonwahab@gmail.com>
2020-09-24 17:54:55 -0500

    Updated Information:
    User enters two new error formats.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -2,20 +2,24 @@ package adder;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
-            int result = addArguments(args);
-            System.out.println(result);
-        } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
-        }
-    }
+	public static void main(String[] args) {
+		try {
+			int result = addArguments(args);
+			System.out.println(result);
+		} catch (NumberFormatException e) {
+			System.err.println("Integers were not formatted correctly");
+		} catch (IllegalArgumentException e) {
+			System.err.println("Please enter at least two integers");
+		} catch (Exception e) {
+			System.err.println("An unexpected error occurred");
+		}
+	}
 
-    private static int addArguments(String[] args) {
-    	int total = 0;
-    	for(int i=0; i < args.length; i++) {
-    		total += Integer.parseInt(args[i]);
-    	}
-        return total;
-    }
+	private static int addArguments(String[] args) {
+		int total = 0;
+		for (int i = 0; i < args.length; i++) {
+			total += Integer.parseInt(args[i]);
+		}
+		return total;
+	}
 }
